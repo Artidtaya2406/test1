3990213c493a66351a7d45a53cdfd965b9f11ebe
Artidtaya2406 <artidtaya2406@gmail.com>
2024-03-28 11:27:05 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("Website Developing using Python")
 
 st.header("🧑‍💻 Website Developing using Python 🧑‍💻")
-st.subheader("✨✨ Artidtaya Pannin ✨✨")
+st.subheader("✨💛 Artidtaya Pannin 💛✨")
 st.image('artidtaya.jpg')
